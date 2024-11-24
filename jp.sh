38d9fa4410af557dcefd68fad1998e97014574d3
djbzq <157609109+djbzq@users.noreply.github.com>
2024-11-25 00:18:55 +0800

    Add files via upload

---
+++ jp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 安装第一个 rel_nodeclient
+echo -e "\n\ny\ny" | bash <(curl -fLSs https://api.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 515b6d00-17e4-4663-9b69-e66b0f47ea2b -u https://ny.chunfa.xyz"
+
+# 安装第二个 rel_nodeclient
+echo -e "a\ny\ny" | bash <(curl -fLSs https://api.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 38353cbd-ee0f-4ec7-865c-379fa3f5b5dd -u https://ny.zhuanf.xyz"
