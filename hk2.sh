5099ddb2b8eb9ac396cf8bac3b8645ab07307a55
djbzq <157609109+djbzq@users.noreply.github.com>
2026-04-02 23:32:02 +0800

    Update hk2.sh

--- hk2.sh
+++ hk2.sh
@@ -1,5 +1,12 @@
-# 一键静默安装脚本
+#!/bin/bash
+
+# 安装第一个 rel_nodeclient
+echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t b444b2c8-cf98-4b32-a6cd-113cb6564d4c -u https://ny.zhuanf.xyz"
+
+# 安装第二个 rel_nodeclient
 echo -e "a\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t f4fa0948-e5b9-4bd7-9e65-bdf08671679d -u https://ny.zhuanf.xyz"
-echo -e "b\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t b444b2c8-cf98-4b32-a6cd-113cb6564d4c -u https://ny.zhuanf.xyz"
+
+
+
 
 curl -L https://raw.githubusercontent.com/nezhahq/scripts/main/agent/install.sh -o agent.sh && chmod +x agent.sh && env NZ_SERVER=aip.nanguacloud.com:8008 NZ_TLS=false NZ_CLIENT_SECRET=lwQTc2aPfqQBPuj6FVRLb32cgpsda5g8 NZ_UUID=6fceb9d2-59cd-487c-db50-4458dcdad940 ./agent.sh
