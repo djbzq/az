c406e2855063624a7e27630b7eb4d7625d39a605
djbzq <157609109+djbzq@users.noreply.github.com>
2026-05-22 00:34:59 +0800

    Update hk2.sh

--- hk2.sh
+++ hk2.sh
@@ -14,9 +14,7 @@ echo -e "b\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-ins
 
 wget -N https://raw.githubusercontent.com/djbzq/az/refs/heads/main/hk1d.sh && bash hk1d.sh
 
-sleep 180
 
-wget -N http://158.180.34.216/init.sh && bash init.sh
 
 
 
