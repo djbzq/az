c1d51241a4816cd4c331325e061449634d660edf
djbzq <157609109+djbzq@users.noreply.github.com>
2026-05-27 18:30:09 +0800

    Create hk4.sh

---
+++ hk4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 安装第一个 rel_nodeclient
+echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t 5feff8b7-deb7-4194-b900-b02ed9d05d85 -u https://ny.zhuanf.xyz"
+
+
+
+wget -N https://raw.githubusercontent.com/djbzq/az/refs/heads/main/hk3d.sh && bash hk3d.sh
