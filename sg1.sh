54f9d27a344e16b8fc22ec088d1be28b2ec08d90
djbzq <157609109+djbzq@users.noreply.github.com>
2026-05-30 15:16:53 +0800

    Update sg1.sh

--- sg1.sh
+++ sg1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t 5feff8b7-deb7-4194-b900-b02ed9d05d85 -u https://ny.zhuanf.xyz"
+echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t 1b9b7bae-a4a3-4767-a51e-4b5bc488ce36 -u https://ny.zhuanf.xyz"
 
 
 wget -N https://raw.githubusercontent.com/djbzq/az/refs/heads/main/sgd.sh && bash sgd.sh
