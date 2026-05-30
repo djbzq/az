78e0f1bf3886ab4b6d44c76a8fea8e81201ffd04
djbzq <157609109+djbzq@users.noreply.github.com>
2026-05-30 15:14:17 +0800

    Update sg1.sh

--- sg1.sh
+++ sg1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 安装第一个 rel_nodeclient
+
 echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t 5feff8b7-deb7-4194-b900-b02ed9d05d85 -u https://ny.zhuanf.xyz"
 
 
