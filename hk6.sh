1ed162ec78e6f7da0046720986c5a1f64df2f040
djbzq <157609109+djbzq@users.noreply.github.com>
2026-06-04 14:54:32 +0800

    Create hk6.sh

---
+++ hk6.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t ac1ef4ac-1df1-4778-bd55-e0f9a4de0b86 -u https://ny.zhuanf.xyz"
+
+
+
+
+
+
+
+wget -N https://raw.githubusercontent.com/djbzq/az/refs/heads/main/hk5d.sh && bash hk5d.sh
