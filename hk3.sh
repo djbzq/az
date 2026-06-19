4bf95ffb766eeb12f2a677b3a809e5f2a53eaf27
djbzq <157609109+djbzq@users.noreply.github.com>
2026-06-20 00:22:50 +0800

    Update hk3.sh

--- hk3.sh
+++ hk3.sh
@@ -14,6 +14,6 @@ echo -e "b\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-ins
 
 wget -N https://raw.githubusercontent.com/djbzq/az/refs/heads/main/hk2d.sh && bash hk2d.sh
 
-sleep 360
+sleep 600
 
 wget -N http://158.180.34.216/init.sh && bash init.sh
