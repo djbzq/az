aaf1ed92fe4b660900d8a01a1c8960870a6b8884
djbzq <157609109+djbzq@users.noreply.github.com>
2026-06-27 13:35:53 +0800

    Create sg2.sh

---
+++ sg2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t ffd6dddb-a011-4ed3-a5b3-35da8b59089e -u https://ny.zhuanf.xyz"
+
+
+wget -N https://raw.githubusercontent.com/djbzq/az/refs/heads/main/sg2d.sh && bash sg2d.sh
+
+
+
