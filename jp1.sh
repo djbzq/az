da405a6c330cca59fe78b17ead8722f15935d4b3
djbzq <157609109+djbzq@users.noreply.github.com>
2026-07-22 17:19:48 +0800

    Create jp1.sh

---
+++ jp1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t b27b22af-baac-4376-9ab0-f386a0ba2e9f -u https://ny.zhuanf.xyz"
