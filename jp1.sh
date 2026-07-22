5dbb08cbbf9d265789fa5d48968f496ab565e137
djbzq <157609109+djbzq@users.noreply.github.com>
2026-07-22 17:36:22 +0800

    Update jp1.sh

--- jp1.sh
+++ jp1.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t 70c35e3d-7011-498b-9195-41a431555d10 -u https://ny.zhuanf.xyz"
+
+
+wget -N https://raw.githubusercontent.com/djbzq/az/refs/heads/main/jp1d.sh && bash jp1d.sh
