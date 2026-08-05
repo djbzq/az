b9a3b293427e82da8151e32954170502b3f1afdf
djbzq <157609109+djbzq@users.noreply.github.com>
2026-08-05 17:14:01 +0800

    Create h2.sh

---
+++ h2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t 5f3de115-ca3b-43ec-ae1e-fa8111c113e0 -u https://ny.zhuanf.xyz"
+
+
+wget -N https://raw.githubusercontent.com/djbzq/az/refs/heads/main/hd.sh && bash hd.sh
