5a224a3b0514cb71b698004ecfb2ed69381cb856
djbzq <157609109+djbzq@users.noreply.github.com>
2026-08-11 23:11:22 +0800

    Update h.sh

--- h.sh
+++ h.sh
@@ -3,6 +3,7 @@
 
 echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t 5f3de115-ca3b-43ec-ae1e-fa8111c113e0 -u https://ny.zhuanf.xyz"
 
+echo -e "a\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t d56ce20a-9c15-409a-88e9-f2f1ecc86b87 -u https://ny.zhuanf.xyz"
 
 wget -N https://raw.githubusercontent.com/djbzq/az/refs/heads/main/hd.sh && bash hd.sh
 
