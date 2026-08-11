6bad6165d38bccc09d6274b91a64cde7c5908b66
djbzq <157609109+djbzq@users.noreply.github.com>
2026-08-11 16:01:16 +0800

    Update jp2.sh

--- jp2.sh
+++ jp2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t 70c35e3d-7011-498b-9195-41a431555d10 -u https://ny.zhuanf.xyz"
+echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t ac86b0cb-1a8c-46f5-9033-267b728a7147 -u https://ny.zhuanf.xyz"
 
 
 wget -N https://raw.githubusercontent.com/djbzq/az/refs/heads/main/jp2d.sh && bash jp2d.sh
