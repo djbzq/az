0c2d771e5046a4edddf71ecdea7cd56382c1aa2a
djbzq <157609109+djbzq@users.noreply.github.com>
2026-09-03 14:39:29 +0800

    Update h2.sh

--- h2.sh
+++ h2.sh
@@ -3,6 +3,8 @@
 
 echo -e "\n\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t 742711ca-a9d3-4233-8a5f-7170d8bcfc67 -u https://ny.zhuanf.xyz"
 
+echo -e "a\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t f4fa0948-e5b9-4bd7-9e65-bdf08671679d -u https://ny.zhuanf.xyz"
+
 
 
 
